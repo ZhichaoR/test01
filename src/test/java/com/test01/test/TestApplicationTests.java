59232eb97bc9a7e491daec42e4a0b198d6bd6e7b
ASUS <2604452386@qq.com>
2023-03-31 12:48:58 +0800

    1111

---
+++ src/test/java/com/test01/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test01.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
